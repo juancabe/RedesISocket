b55896f43b75091f5b1a0fec86813a73d3169c9e
juancabe <calzadaj@hotmail.es>
2024-12-05 23:54:54 +0100

    tests for parse_client_request.h

--- src/servidor/parse_client_request.h
+++ src/servidor/parse_client_request.h
@@ -3,6 +3,8 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 typedef enum
 {
@@ -40,9 +42,92 @@ static bool check_barw(char *ptr)
 
 static bool check_at_whole_str(char *ptr)
 {
-  while (*(ptr) != '\0' && *(ptr++) != '@')
-    ;
-  return *ptr == '\0';
+  while (*(ptr) != '\0' && *(ptr) != '@')
+  {
+    ptr++;
+  }
+  return *ptr != '\0';
+}
+
+// Expecting a hostname ex: @hostname
+static int STATE_hostname(
+    char *ptr,
+    char **out_username,
+    char **out_hostname,
+    bool *username_set_out,
+    bool *hostname_set_out,
+    bool must_have_hostname)
+{
+  if (*ptr != '@')
+  {
+    return ERROR;
+  }
+
+#ifdef DEBUG
+  printf("[STATE_hostname] till here\n");
+#endif
+
+  ptr++;
+
+  if (check_CRLF(ptr) || check_end(ptr))
+  {
+    return ERROR;
+  }
+
+  char *hostname_start = ptr;
+
+  while (*ptr != ' ' && !check_CRLF(ptr) && !check_end(ptr))
+  {
+    ptr++;
+  }
+
+  // Now ptr is pointing to a space, CRLF or end of string
+  // Only CRLF is valid
+
+  // Check that we read something
+  if (ptr == hostname_start)
+  {
+    return ERROR;
+  }
+
+#ifdef DEBUG
+  printf("[STATE_hostname] hostname exists\n");
+#endif
+
+  if (check_CRLF(ptr)) // Check CRLF
+  {
+#ifdef DEBUG
+    printf("[STATE_hostname] CRLF -> ");
+#endif
+    *out_hostname = malloc(ptr - hostname_start + 1);
+    if (*out_hostname == NULL)
+      return ERROR;
+    strncpy(*out_hostname, hostname_start, ptr - hostname_start);
+    (*out_hostname)[ptr - hostname_start] = '\0';
+
+    *hostname_set_out = true;
+    if (must_have_hostname)
+    {
+#ifdef DEBUG
+      printf("HOSTNAME_REDIRECT\n");
+#endif
+      return HOSTNAME_REDIRECT;
+    }
+    else
+    {
+#ifdef DEBUG
+      printf("ERROR\n");
+#endif
+      return ERROR;
+    }
+  }
+  else
+  {
+#ifdef DEBUG
+    printf("[STATE_hostname] CRLF not found\n");
+#endif
+    return ERROR;
+  }
 }
 
 // Expecting a username ex: i0960231
@@ -80,21 +165,37 @@ static int STATE_username(
     return ERROR;
   }
 
+#ifdef DEBUG
+  printf("[STATE_username] username exists\n");
+#endif
+
   if (check_CRLF(ptr)) // Check CRLF
   {
+#ifdef DEBUG
+    printf("[STATE_username] CRLF -> ");
+#endif
     *out_username = username_start;
     *username_set_out = true;
     if (must_have_hostname)
     {
+#ifdef DEBUG
+      printf("ERROR\n");
+#endif
       return ERROR;
     }
     else
     {
+#ifdef DEBUG
+      printf("NO_USERNAME_NO_HOSTNAME\n");
+#endif
       return USERNAME;
     }
   }
   else if (*ptr == '@') // Check @
   {
+#ifdef DEBUG
+    printf("[STATE_username] @ -> ");
+#endif
     *out_username = malloc(ptr - username_start + 1);
     if (*out_username == NULL)
       return ERROR;
@@ -102,13 +203,18 @@ static int STATE_username(
     (*out_username)[ptr - username_start] = '\0';
 
     *username_set_out = true;
-    ptr++;
     if (check_CRLF(ptr) || check_end(ptr) || skip_spaces(&ptr))
     {
+#ifdef DEBUG
+      printf("ERROR\n");
+#endif
       return ERROR;
     }
     else
     {
+#ifdef DEBUG
+      printf("goto hostname\n");
+#endif
       return STATE_hostname(ptr, out_username, out_hostname, username_set_out, hostname_set_out, must_have_hostname);
     }
   }
@@ -118,64 +224,26 @@ static int STATE_username(
   }
 }
 
-// Expecting a hostname ex: @hostname
-static int STATE_hostname(
-    char *ptr,
-    char **out_username,
-    char **out_hostname,
-    bool *username_set_out,
-    bool *hostname_set_out,
-    bool must_have_hostname)
+static int username_or_hostname(char *ptr, char **out_username, char **out_hostname, bool *username_set_out, bool *hostname_set_out, bool must_have_hostname)
 {
-  if (*ptr != '@')
-  {
-    return ERROR;
-  }
-
-  ptr++;
-
   if (check_CRLF(ptr) || check_end(ptr))
   {
     return ERROR;
   }
 
-  char *hostname_start = ptr;
-
-  while (*ptr != ' ' && !check_CRLF(ptr) && !check_end(ptr))
+  if (*ptr == '@')
   {
-    ptr++;
-  }
-
-  // Now ptr is pointing to a space, CRLF or end of string
-  // Only CRLF is valid
-
-  // Check that we read something
-  if (ptr == hostname_start)
-  {
-    return ERROR;
-  }
-
-  if (check_CRLF(ptr)) // Check CRLF
-  {
-    *out_hostname = malloc(ptr - hostname_start + 1);
-    if (*out_hostname == NULL)
-      return ERROR;
-    strncpy(*out_hostname, hostname_start, ptr - hostname_start);
-    (*out_hostname)[ptr - hostname_start] = '\0';
-
-    *hostname_set_out = true;
-    if (must_have_hostname)
-    {
-      return HOSTNAME_REDIRECT;
-    }
-    else
-    {
-      return ERROR;
-    }
+#ifdef DEBUG
+    printf("[username_or_hostname] Detected hostname\n");
+#endif
+    return STATE_hostname(ptr, out_username, out_hostname, username_set_out, hostname_set_out, must_have_hostname);
   }
   else
   {
-    return ERROR;
+#ifdef DEBUG
+    printf("[username_or_hostname] Detected username\n");
+#endif
+    return STATE_username(ptr, out_username, out_hostname, username_set_out, hostname_set_out, must_have_hostname);
   }
 }
 
@@ -186,6 +254,8 @@ int parse_client_request(char *in_buf, char *out_hostname, char *out_username)
   bool username_set_out = false;
   bool hostname_set_out = false;
   bool must_have_hostname = false;
+  out_hostname = NULL;
+  out_username = NULL;
 
   if (ptr == NULL || check_end(ptr))
   {
@@ -197,6 +267,10 @@ int parse_client_request(char *in_buf, char *out_hostname, char *out_username)
     must_have_hostname = true;
   }
 
+#ifdef DEBUG
+  printf("The input %s\n", must_have_hostname ? "MUST have hostname" : "MUST NOT have hostname");
+#endif
+
   // It must start with anything but a space
   if (*ptr == ' ')
   {
@@ -207,24 +281,39 @@ int parse_client_request(char *in_buf, char *out_hostname, char *out_username)
 
   if (check_barw(ptr))
   {
+#ifdef DEBUG
+    printf("ERROR 1\n");
+#endif
     ptr += 2;
     // After /W, it can be a space or a CRLF
     if (skip_spaces(&ptr))
     {
       // after spaces, it can be username or hostname, no CRLF or \0
-      if (check_CRLF(ptr) || check_end(ptr))
+      return username_or_hostname(ptr, &out_username, &out_hostname, &username_set_out, &hostname_set_out, must_have_hostname);
+    }
+    else
+    {
+      // It can be just /W, so now it must be CRLF
+      if (check_CRLF(ptr))
       {
-        return ERROR;
-      }
-      else if ((*ptr) == '@')
-      {
-        return STATE_hostname(ptr, &out_username, &out_hostname, &username_set_out, &hostname_set_out, must_have_hostname);
-        exit(-1);
+        return NO_USERNAME_NO_HOSTNAME;
       }
       else
       {
-        return STATE_username(ptr, &out_username, &out_hostname, &username_set_out, &hostname_set_out, must_have_hostname);
+        return ERROR;
       }
+    }
+  }
+  else
+  {
+    // If no /W, it can be a username or hostname or CRLF
+    if (check_CRLF(ptr))
+    {
+      return NO_USERNAME_NO_HOSTNAME;
+    }
+    else
+    {
+      return username_or_hostname(ptr, &out_username, &out_hostname, &username_set_out, &hostname_set_out, must_have_hostname);
     }
   }
 }
