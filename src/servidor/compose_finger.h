753915fff96fe9884a68c3095446d01f50a5d132
juancabe <calzadaj@hotmail.es>
2024-12-06 16:20:38 +0100

    Prueba

--- src/servidor/compose_finger.h
+++ src/servidor/compose_finger.h
@@ -420,12 +420,12 @@ char *just_one_user_info(char *username)
   {
     if (ut->ut_type == USER_PROCESS && strncmp(ut->ut_user, username, UT_NAMESIZE) == 0)
     {
-      printf("Found user %s\n", ut->ut_user);
       UUTX_array_add(&users_array, ut);
     }
   }
   endutxent();
 
+  printf("Count: %d\n", users_array.count);
   for (int i = 0; i < users_array.count; i++)
   {
     char *user_str = user_info(users_array.users[i].username, &(users_array.users[i]));
