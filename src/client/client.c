62be853da011dae7eca3438d8c04236688361c31
juancabe <calzadaj@hotmail.es>
2024-12-06 22:22:29 +0100

    Changes

--- src/client/client.c
+++ src/client/client.c
@@ -2,15 +2,16 @@
 
 int main(int argc, char **argv)
 {
-  if (argc != 3)
+  if (argc < 2 || argc > 3)
   {
     fprintf(stderr, "uso: %s {TCP|UDP} [request]\n", argv[0]);
     exit(1);
   }
 
   // Add \r\n to the request
-  char *request = malloc(strlen(argv[2]) + 3);
-  strcpy(request, argv[2]);
+  char *request = NULL;
+  request = malloc((argc == 3 ? strlen(argv[2]) : 0) + 3);
+  strcpy(request, argc == 3 ? argv[2] : "");
   strcat(request, "\r\n");
 
   if (strcmp(argv[1], "TCP") == 0)
