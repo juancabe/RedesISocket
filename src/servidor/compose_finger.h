93abdd825d21709721ee97b677f81b3360b63301
juancabe <calzadaj@hotmail.es>
2024-12-09 23:05:06 +0100

    600 mb

--- src/servidor/compose_finger.h
+++ src/servidor/compose_finger.h
@@ -43,7 +43,7 @@ char *just_one_user_info(char *username) {
 
 #elif defined(SEND_BIG_CHUNK)
 
-const int CHUNK_SIZE = 96388608; // 960 mb
+const int CHUNK_SIZE = 66388608; // 960 mb
 
 char *all_users_info() {
   char *info = (char *)malloc(CHUNK_SIZE); // 900KB
