ae23ed1526f8c01c34236c83318db1ea86c2897f
juancabe <calzadaj@hotmail.es>
2024-12-09 20:30:28 +0100

    a

--- src/cliente/cliente.c
+++ src/cliente/cliente.c
@@ -30,6 +30,7 @@ int main(int argc, char **argv) {
   } else {
 #ifdef SEND_BIG_CHUNK
     printf("Response length: %ld\n", strlen(response));
+    printf("%s", response);
 #else
     printf("%s", response);
 #endif
